256cc2799adef99b433263c073aca6561a38c712
philippe-cuaz <philippe.cuaz@orange.fr>
2021-06-25 13:07:26 +0200

    Modification of the geolaction API key

--- src/util/forecast.js
+++ src/util/forecast.js
@@ -1,14 +1,17 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
-
+    const url2 = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+     const url='https://api.agromonitoring.com/agro/1.0/weather?lat='+latitude+'&lon='+longitude+'&appid=51cd40cbc1db0101d3f93acbc358a0ce'
     request({ url, json: true }, (error, { body }) => {
         if (error) {
+          console.log(body)
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
+          console.log(body)
             callback('Unable to find location', undefined)
         } else {
+          console.log(body)
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
--- src/util/geocode.js
+++ src/util/geocode.js
@@ -1,11 +1,12 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
-    const url1="https://maps.googleapis.com/maps/api/geocode/json?Sussex%20Drive%20Ottawa%20ON"
+  const key='pk.eyJ1IjoicGhpbGlwcGVjdWF6IiwiYSI6ImNrcWM4MXNhaTB1NnEyb292MTE0cWcydGsifQ.ic_-sksCTSQk1PTaT3M3Lw'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token='+key+'&limit=1'
+  //  const url1="https://maps.googleapis.com/maps/api/geocode/json?Sussex%20Drive%20Ottawa%20ON"
   //  const url2="https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key="
-    const url2="https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key=AIzaSyCnUx1sH7KScEXqyaF5eg2oigAic7LmDl4"
-    const apikey="AIzaSyCnUx1sH7KScEXqyaF5eg2oigAic7LmDl4"
+  //  const url2="https://maps.googleapis.com/maps/api/geocode/json?address=24%20Sussex%20Drive%20Ottawa%20ON&key=AIzaSyCnUx1sH7KScEXqyaF5eg2oigAic7LmDl4"
+//    const apikey="AIzaSyCnUx1sH7KScEXqyaF5eg2oigAic7LmDl4"
 
     request({ url, json: true }, (error,  body ) => {
   //    console.log('body: '+body)
@@ -22,7 +23,7 @@ const geocode = (address, callback) => {
             })
         }
     })
-    
+
 }
 
 module.exports = geocode
